24c0bb140414899786731164db923c5679d07321
Marvin <mjacobsz@gmail.com>
2019-05-13 13:17:17 +0200

    Introduce a method that can chain 'where' clauses together

--- app/models/mushroom.rb
+++ app/models/mushroom.rb
@@ -1,2 +1,12 @@
 class Mushroom < ApplicationRecord
+  def self.chain_where(conditions)
+    conditions.inject(self) do |mushroom_collection, condition|
+      query_column = condition[0] # i.e. 'edible-status'
+      query_value  = condition[1] # i.e. 'poisonous'
+
+      column_value_hash = Hash.new
+      column_value_hash[query_column] = query_value
+      mushroom_collection.where(column_value_hash)
+    end
+  end
 end
